adfd97c7ad3510eebc344045738e587a4306d63b
MBAH AGWO TABI <mbah_agwo@preprint.cm>
2023-08-09 15:01:11 +0100

    local changes

--- src/main/java/presprint/task/manager/backend/controller/TaskController.java
+++ src/main/java/presprint/task/manager/backend/controller/TaskController.java
@@ -1,9 +1,15 @@
 package presprint.task.manager.backend.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import presprint.task.manager.backend.entity.Task;
+import presprint.task.manager.backend.form.TaskDTO;
 
 @RestController
 @RequestMapping("/api/v1/task")
@@ -12,6 +18,32 @@ public class TaskController {
 	@RequestMapping(value="/test", method =RequestMethod.GET)
 	public String sayHello(@RequestParam("message") String message) {
 		return "Hello Sir="+message;
+	}
+
+	@RequestMapping(value="/create", method = RequestMethod.POST)
+	public Task create(@RequestBody TaskDTO taskDTO){
+
+		Task task = new Task();
+		task.setTitle(taskDTO.getTitle());
+		task.setDescription(taskDTO.getDescription());
+		task.setDuration(taskDTO.getDuration());
+		task.setStatus(statusService.findByName("OPENED"));
+		taskRepository.save(task);
+
+		return new ResponseEntity<Object>(result, HttpStatus.CREATED);
+	}
+
+	@RequestMapping(value="/update", method = RequestMethod.POST)
+	public Task update(@RequestBody TaskDTO taskDTO){
+
+		Task task = new Task();
+		task.setTitle(taskDTO.getTitle());
+		task.setDescription(taskDTO.getDescription());
+		task.setDuration(taskDTO.getDuration());
+		task.setStatus(statusService.findByName("OPENED"));
+		taskRepository.save(task);
+
+		return new ResponseEntity<Object>(result, HttpStatus.CREATED);
 	}
 
 }
