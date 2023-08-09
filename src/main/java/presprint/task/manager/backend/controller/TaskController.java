180ae518c12ca7d0c7713906e1269acfcfe7021d
radius <radius@presprint.cm>
2023-08-09 10:58:23 +0100

    INITIAL COMMIT

---
+++ src/main/java/presprint/task/manager/backend/controller/TaskController.java
@@ -0,0 +1,17 @@
+package presprint.task.manager.backend.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class TaskController {
+	
+	@RequestMapping(value="/test", method =RequestMethod.GET)
+	public String sayHello(@RequestParam("message") String message) {
+		return "Hello Sir="+message;
+	}
+
+}
