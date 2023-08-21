d827ebb4604a8122fed360f1a7d539e1d1de0f05
Frankj23 <112391319+Frankj23@users.noreply.github.com>
2023-08-21 13:53:18 +0100

    completed the js function of the new tast page left with the implementation of the duration

--- Home/register/register.js
+++ Home/register/register.js
@@ -1,38 +1,46 @@
 // function submitbtn(){
     var title = document.getElementById('title');
-    var description = document.getElementById('Description');
+    var description = document.getElementById('description');
     var duration = document.getElementById('duration');
-    var form = document.getElementById('form');
-    console.log("field cannot be blank!");
+    // var form = document.getElementById('form');
+    console.log("fields cannot be blank!");
     
     document.querySelector('button').addEventListener('click', (event)=>{
         event.preventDefault();
         validateInput();
     });
     function validateInput(){
-        // if (title.value.trim()===""){
-        //     onError(title, "Please write the title")
-        // }
-        // else{
-        //     onSuccess(description)
-        // }
-        if(description.value.trim() === "" ){
-            onError(description, "you forgot the description")
+        if (title.value.trim()===""||title.value.trim()===null){
+            onError(title, "Please write the title");
+        }
+        else if(title.value.trim().length<3){
+            onError(title, "the title cannot have less than 3 characters");
         }
         else{
+            onSuccess(title);
+        };
+        if (description.value.trim()===""||description.value.trim()===null) {
+            onError(description, "you forgot the description");
+        } else {
             onSuccess(description);
-        }
-    }
+        };
+//         if(duration.value<0){
+//             onError(duration, "the duration cannot be a negative number");
+//         }else{
+//             onSuccess(duration);
+//         }
+};
     function onSuccess(input){
-            let parent = title.parentElement;
+            let parent = input.parentElement;
             let messageElement = parent.querySelector('small');
+            document.querySelector('i').style.border= '1px solid green';
             messageElement.style.visibility= 'hidden';
             messageElement.innerText=input;
     }
     function onError(input, message){
-            let parent = title.parentElement;
+            let parent = input.parentElement;
             let messageElement = parent.querySelector('small');
-            document.querySelector('i').style.border= '1px solid red';
+            // document.querySelector('i').style.border= '1px solid red';
             messageElement.style.visibility= 'visible';
             messageElement.innerText=message;
     }
